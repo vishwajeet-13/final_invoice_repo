5c65a2f1f372b4a4b87b01a4497eb6b438830883
vishwajeet-13 <vishwajeetthakur403@gmail.com>
2025-06-24 11:42:32 +0530

    fix: updated allowed_origins

--- backend/inv/settings.py
+++ backend/inv/settings.py
@@ -64,9 +64,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:5173"
+  "https://final-invoice-repo.onrender.com"
 ]
 
 
